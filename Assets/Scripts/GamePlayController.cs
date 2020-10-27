024d90ea7dae30c168b46e4b4785ef3250a4473a
Сергей Лишанков <tensior@yandex.ru>
2020-10-27 12:10:02 +0300

    Начальный коммит
    Настроил сцену, добавил фреймворк StrangeIoC

---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GamePlayController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _ball;
+    [SerializeField] private Vector2Int _ballSpeedLimits;
+
+    private void Awake()
+    {
+        _ball.velocity = GetRandomVelocity(_ballSpeedLimits);
+    }
+
+    private Vector2 GetRandomVelocity(Vector2 limits)
+    {
+        var angle = Random.Range(0f, Mathf.PI);
+        var value = Random.Range(limits.x, limits.y);
+        return new Vector2(Mathf.Cos(angle) * value, Mathf.Sin(angle) * value);
+    }
+}
